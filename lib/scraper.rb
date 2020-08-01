5ccd87f7d9dad12a409d7c222af35c82f382a792
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-08-01 00:48:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,11 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
+  end
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -17,6 +22,6 @@ class Scraper
   end
   
 end
-
+Scraper.new.get_page
 
 
